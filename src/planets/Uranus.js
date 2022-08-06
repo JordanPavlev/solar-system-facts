ec22b8111e0b9c9b799839d35dbb54a609c29a9b
JordanPavlev <danipavlev@gmail.com>
2022-08-06 23:57:26 +0300

    added planets data TODO Section

---
+++ src/planets/Uranus.js
@@ -0,0 +1,3 @@
+import earthImage from '../../assets/images/planet-earth.svg';
+import earthInternalImage from '../../assets/images/planet-earth-internal.svg';
+import earthGeoImage from '../../assets/images/planet-earth-geology.png';
